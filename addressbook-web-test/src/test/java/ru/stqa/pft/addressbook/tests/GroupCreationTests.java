e3325c4d3517f60450105e814c2c0ca1a283989f
Vlad Petrov <retqqqq@gmail.com>
2018-06-15 18:19:08 +0300

    3-level structure composed

---
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTests extends TestBase {
+
+    @Test
+    public void testGropupCreation() {
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupForm();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
